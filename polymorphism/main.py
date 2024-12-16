5be5a1e6a3d5d4dca9d942cbccd3780ff207fc06
mecoder1412 <imaniwambui2023@gmail.com>
2024-12-16 16:02:43 +0300

    Add files via upload

---
+++ polymorphism/main.py
@@ -0,0 +1,16 @@
+class ferrari:
+    def horse_power(self):
+        print("Its horse power is ",460,"to",963)
+    def c_lass(self):
+        print("Its car classes are Grand tourer and Sports car")    
+class lamborghini:
+    def horse_power(self):
+        print("Its horse power is ",550,"to",700)
+    def c_lass(self):
+        print("Its car class is Sports car")
+ap=ferrari()
+ap.horse_power()
+ap.c_lass()
+lem=lamborghini() 
+lem.horse_power()
+lem.c_lass()      
