903182bdc4af13956619695d579ce2ff32320652
mecoder1412 <imaniwambui2023@gmail.com>
2024-12-16 15:18:38 +0300

    Add files via upload

---
+++ Abstraction/poly.py
@@ -0,0 +1,16 @@
+class apple:
+    def look(self):
+        print("An apple is red or green")
+    def taste(self):
+        print("It has a sweet taste")    
+class lemon:
+    def look(self):
+        print("A lemon is yellow")
+    def taste(self):
+        print("It has a sour taste")
+ap=apple()
+ap.look()
+ap.taste()
+lem=lemon() 
+lem.look()
+lem.taste()                   
